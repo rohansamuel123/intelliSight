a53d6bcec749ba22a16bee747712da487f5b6904
zuber144 <zzuber144@gmail.com>
2026-05-03 02:26:02 +0530

    feat: enhance Index component to display API response with improved styling

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,21 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import API from "../services/api";
 
 export default function Index() {
+  const [res, setRes] = useState({})
   useEffect(() => {
     API.get("/")
       .then((res) => {
+        setRes(res.data)
         console.log("Backend:", res.data);
       })
       .catch((err) => {
+        setRes(err.message)
         console.log("Error:", err.message);
       });
   }, []);
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home Screen (Check console)</Text>
-    </View>
+    <View style={{ 
+    flex: 1,                 // Fill the whole screen
+    justifyContent: "center", // Vertical center
+    alignItems: "center",     // Horizontal center
+    padding: 20              // Prevents text from hitting screen edges
+  }}>
+    <Text style={{ fontWeight: 'bold', marginBottom: 10 }}>
+      Home Screen (Check console)
+    </Text>
+    
+    <Text style={{ textAlign: 'center', color: '#555' }}>
+      No Need to check console, it's here:
+    </Text>
+    
+    {/* Displaying the data in a cleaner way */}
+    <Text style={{ 
+      marginTop: 10, 
+      backgroundColor: '#f0f0f0', 
+      padding: 10, 
+      borderRadius: 5,
+      fontFamily: 'monospace' // Makes it look like code
+    }}>
+      {JSON.stringify(res, null, 2)} 
+    </Text>
+</View>
+
   );
 }
